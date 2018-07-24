d837711f556abe6505481670307ebeb5981087be
Andrew <andrew.vinocur@gmail.com>
2018-07-23 18:29:01 -0700

    advances app progress with button

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print ('Hi There');
+        },
+      ),
       appBar: AppBar(
         title: Text('Lets see some images!'),
       ),
