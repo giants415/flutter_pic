80d9163681fc0c2ffe554c163feeaad47c29b774
Andrew <andrew.vinocur@gmail.com>
2018-07-23 17:08:19 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+// I need to import a helper library from flutter to help get content on screen
+
+
+//Define a 'main' fn to run when app starts
+// Create a new text widget to show some text on screen
+// Take widget and get it on the screen
