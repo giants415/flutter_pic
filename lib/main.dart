6c82d0af7b1c1ac2fdc5a700d6e027c0db53ced2
Andrew <andrew.vinocur@gmail.com>
2018-07-23 17:51:19 -0700

    removes comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
-// I need to import a helper library from flutter to help get content on screen
 import 'package:flutter/material.dart';
 
-
-//Define a 'main' fn to run when app starts
 void main() {
-  // Create a new text widget to show some text on screen
   var app = MaterialApp(
     home: Text('Hi there!')
   );
 
 
-  // Take widget and get it on the screen
   runApp(app);
 }
