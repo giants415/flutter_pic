de72504dfb93542f1b13d5fd337c235f6f4f9050
Andrew <andrew.vinocur@gmail.com>
2018-07-25 10:53:59 -0700

    pastes in material app code

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -6,7 +6,19 @@ import 'package:flutter/material.dart';
 class App extends StatelessWidget {
   // Must define a 'build' method that returns the widgets that *this* widget will show
   Widget build(context) {
-    
+    return MaterialApp(
+    home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print ('Hi There');
+        },
+      ),
+      appBar: AppBar(
+        title: Text('Lets see some images!'),
+      ),
+    ),
+  );
   }
 }
 
