e2539ed56f1e78e1733684b2842d1916a4051211
Andrew <andrew.vinocur@gmail.com>
2018-07-25 10:51:41 -0700

    builds out stateless widget structure

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,5 +3,10 @@ import 'package:flutter/material.dart';
 
 // Create a class that will be our custom widget
 // This class must extend the 'StatelessWidget' base class
+class App extends StatelessWidget {
+  // Must define a 'build' method that returns the widgets that *this* widget will show
+  Widget build(context) {
+    
+  }
+}
 
-// Must define a 'build' method that returns the widgets that *this widget will show
