dc83b5511e78cf04f8c8ac54eaf0ac88590ee880
Andrew <andrew.vinocur@gmail.com>
2018-07-27 13:59:22 -0700

    sets List as final

---
+++ lib/src/widgets/image_list.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../models/img_model.dart';
+
+class ImageList extends StatelessWidget {
+  final List<ImageModels> images;
+
+  ImageList(this.images);
+}
