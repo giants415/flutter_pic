d61c6946d21d644bdf1576e72075863e867f2d34
Andrew <andrew.vinocur@gmail.com>
2018-07-27 16:14:38 -0700

    refactors to have padding on image

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -22,10 +22,13 @@ class ImageList extends StatelessWidget {
       ),
       margin: EdgeInsets.all(20.0),
       padding: EdgeInsets.all(20.0),
-      child: Column( 
-        children: <Widget> [
-          Image.network(image.url),
-          Text(image.title)
+      child: Column(
+        children: <Widget>[
+          Padding(
+            child: Image.network(image.url),
+            padding: EdgeInsets.only(bottom: 8.0)
+          ),
+          Text(image.title),
         ],
       ),
     );
